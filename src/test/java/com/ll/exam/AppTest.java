2b718fa29ce326892191f96590a90e82c838c82c
jinsu291 <wlstn291@naver.com>
2022-07-05 13:29:08 +0900

    세팅2

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,12 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void 더하기_테스트() {
+        int rs = Calculator.add("10 + 20");
+
+       asserte
+    }
+}
