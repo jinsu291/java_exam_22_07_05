aeafe254df90877041c66c59efc6d6698310c398
jinsu291 <wlstn291@naver.com>
2022-07-05 14:03:00 +0900

    테스트 케이스 2개,더하기 기능

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -2,11 +2,21 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppTest {
     @Test
     public void 더하기_테스트() {
         int rs = Calculator.add("10 + 20");
 
-       asserte
+
+        assertEquals(30, rs);
+    }
+    @Test
+    public void 더하기_테스트_2() {
+        int rs = Calculator.add("20 + 20");
+
+
+        assertEquals(40, rs);
     }
 }
